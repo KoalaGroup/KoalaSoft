4b57f67e4a72e7ad3923ecf07ef1f8ed1f456fff
Stefan Pflüger <stpflueg@uni-mainz.de>
2020-05-19 15:18:09 +0200

    ci!: fix and improve CI configuration
    * Make pipelines trigger on forks again. This is how gitlab Merge Requests
    pipelines are set up. Note that the individual runners have to be shared
    with the individual users who perform merge requests.
    * Add before script stage to setup environment correctly
    * Add test stage and improve config with template. Note the usage of
    artifacts, which allows sharing of the build files with the test job.

--- test/KoaTest.cxx
+++ test/KoaTest.cxx
@@ -1,10 +1,18 @@
+#define BOOST_TEST_MODULE ExampleTest
+
 #include "KoaTest.h"
 #include <iostream>
 // #include "KoaMapEncoder.h"
 
-void KoaTest::Print()
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(ExampleTest)
+
+BOOST_AUTO_TEST_CASE(FirstTestApp)
 {
   // auto encoder = KoaMapEncoder::Instance();
   // auto i = encoder->GetRecRearID();
   std::cout << "I'm KoaTest!" << std::endl;
 }
+
+BOOST_AUTO_TEST_SUITE_END()
