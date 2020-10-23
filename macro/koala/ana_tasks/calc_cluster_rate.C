05f39328be742cbbb90fdcd373f5e7254c2c05b1
Yong <y.zhou@fz-juelich.de>
2020-10-23 21:49:10 +0200

    [Backup]

--- macro/koala/ana_tasks/calc_cluster_rate.C
+++ macro/koala/ana_tasks/calc_cluster_rate.C
@@ -6,57 +6,48 @@ using namespace std;
  * the half size of which is provided by user.
  */
 
-void calc_ems_rate(const char* infile,
-                   const char* outfile,
-                   int half_window = 2)
+void calc_cluster_rate(const char* infile,
+                       const char* outfile,
+                       int half_window = 2)
 {
   // 1. read the scalor data into buffer
   auto fin = new TFile(infile);
   
-  auto tin = (TTree*)fin->Get("Ems");
-  KoaEmsRawEvent *ems = nullptr;
-  tin->SetBranchAddress("KoaEmsRawEvent", &ems);
+  auto tin = (TTree*)fin->Get("ClusterSumCount");
+  Long64_t sec, usec;
+  std::map<Int_t, Long_t> *sum_counts = nullptr;
+  tin->SetBranchAddress("Second", &sec);
+  tin->SetBranchAddress("Usecond", &usec);
+  tin->SetBranchAddress("ClusterSumCount", &sum_counts);
 
-  vector<array<UInt_t, 32>> scalor;
-  vector<Long64_t> sec;
-  vector<Long64_t> usec;
+  vector<map<Int_t, Long_t>> counts;
+  vector<Long64_t> secs;
+  vector<Long64_t> usecs;
   vector<double> time;
 
+  // 2. retrieve counts into buffer
   auto entries = tin->GetEntries();
   for(auto entry = 0 ; entry<entries; entry++){
     tin->GetEntry(entry);
 
-    scalor.emplace_back();
-    for(int i = 0; i<32; i++){
-      scalor[entry][i] = ems->Scalor[i];
-    }
-    time.emplace_back(ems->Second+1.e-6*ems->Usecond);
-    sec.emplace_back(ems->Second);
-    usec.emplace_back(ems->Usecond);
+    //
+    time.emplace_back(sec+1.e-6*usec);
+    secs.emplace_back(sec);
+    usecs.emplace_back(usec);
+    counts.emplace_back((*sum_counts));
   }
   cout << "Entries: " << entries << endl;
 
-  // 2. Retrieve the scalor channel mapping
-  map<string,int>* ScalorChMap = nullptr;
-  fin->GetObject("ScalorChMap", ScalorChMap);
-
   // 3. configure the output branch
   auto fout = new TFile(outfile, "update");
-  auto tout = new TTree("EMS_Rates", "Rates from EMS scalor");
+  auto tout = new TTree("KOALA_Cluster_Rates", "Rates from KOALA Cluster");
 
-  double rate[32];
+  std::map<Int_t, double> *rates = new std::map<Int_t,double>();
   Long64_t second;
   Long64_t usecond;
   tout->Branch("Second", &second, "Second/L");
   tout->Branch("Usecond", &usecond, "Usecond/L");
-  for(auto it = ScalorChMap->begin(); it != ScalorChMap->end(); it++) {
-    tout->Branch(Form("%s_Rate",it->first.data()), rate+it->second, Form("%s_Rate/d",it->first.data()));
-  }
-
-  double fwd_InOut_ratio, fwd_UpDown_ratio, daq_efficiency;
-  tout->Branch("Fwd_InOut_Ratio", &fwd_InOut_ratio, "Fwd_InOut_Ratio/d");
-  tout->Branch("Fwd_UpDown_Ratio", &fwd_UpDown_ratio, "Fwd_UpDown_Ratio/d");
-  tout->Branch("DAQ_Efficiency", &daq_efficiency, "DAQ_Efficiency/d");
+  tout->Branch("KOALA_Cluster_Rate", &rates);
 
   // 4. calculate the rate in the event window [-half_window-current, half_window+current]
   auto get_edge_entry = [&](long current, long total, int half = 2, bool decrease_order = false) -> long
@@ -84,27 +75,26 @@ void calc_ems_rate(const char* infile,
                         };
 
   for(auto entry = 0 ; entry<entries; entry++){
+    rates->clear();
+
     auto low = get_edge_entry(entry, entries, half_window, true);
     auto high = get_edge_entry(entry, entries, half_window, false);
     auto dt = time[high] - time[low];
 
-    for(auto it = ScalorChMap->begin(); it != ScalorChMap->end(); it++) {
-      rate[it->second] = (scalor[high][it->second] - scalor[low][it->second])/dt;
+    for(auto item: counts[high]) {
+      auto rate = (item.second - counts[low][item.first])/dt;
+      rates->emplace(item.first, rate);
     }
 
-    second = sec[entry];
-    usecond = usec[entry];
-
-    fwd_InOut_ratio = rate[(*ScalorChMap)["Fwd_Inside"]]/rate[( *ScalorChMap )["Fwd_Outside"]];
-    fwd_UpDown_ratio = rate[( *ScalorChMap )["Fwd_Up"]]/rate[( *ScalorChMap )["Fwd_Down"]];
-    daq_efficiency = rate[( *ScalorChMap )["Trigger"]]/rate[( *ScalorChMap )["CommonOR"]];
+    second = secs[entry];
+    usecond = usecs[entry];
 
     //
     tout->Fill();
   }
 
   // 5. write the output
-  fout->WriteTObject(tout,"","Delete");
+  fout->WriteTObject(tout,"","WriteDelete");
 
   delete fout;
   delete fin;
